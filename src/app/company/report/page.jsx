1176b6bd1b4c022d6cacba8309dd0c34e71748a3
Mushfik Ahmed <ahmed.mushfik16381@gmail.com>
2023-11-08 20:00:51 +0600

    pagination disable

--- src/app/company/report/page.jsx
+++ src/app/company/report/page.jsx
@@ -583,10 +583,33 @@ const Page = () => {
                         </svg>
                         Prev
                       </button>
-
-                      <button
+                      {
+                        service?.data?.length < 10 ?
+                        <button
                         type="button"
-                        className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm      "
+                        disabled
+                        className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-gray-300 text-gray-700 shadow-sm align-middle  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
+                      >
+                        Next
+                        <svg
+                          className="w-3 h-3"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                          />
+                        </svg>
+                      </button>
+                        :
+                        <button
+                        type="button"
+                        className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
+                        
                         onClick={() => setCurrentPage(currentPage + 1)}
                       >
                         Next
@@ -604,6 +627,7 @@ const Page = () => {
                           />
                         </svg>
                       </button>
+                      }
                     </div>
                   </div>
                 </div>
